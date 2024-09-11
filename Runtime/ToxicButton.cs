475af33cf0da6a6e21ea8d979a89e33641d75243
cyhzz <1076357483@qq.com>
2024-09-11 20:34:24 +0800

    Update ToxicButton.cs

--- Runtime/ToxicButton.cs
+++ Runtime/ToxicButton.cs
@@ -11,17 +11,50 @@ namespace Com.A9.ToxicSystem
         [SerializeField]
         ToxicType id;
         Button button;
+        [SerializeField]
+        UnityEvent OnLoadingStart;
+        [SerializeField]
+        UnityEvent OnLoadingComplete;
+        [SerializeField]
+        UnityEvent OnStartWatch;
+        [SerializeField]
+        UnityEvent OnInteractive;
+        [SerializeField]
+        UnityEvent OnDisabled;
 
         void Start()
         {
             button = GetComponent<Button>();
             var ad = ToxicSystem.instance.ads[id];
-            ad.OnLoadingStart += () => { button.interactable = false; };
-            ad.OnLoadingComplete += () => { button.interactable = true; };
-            ad.OnStartWatch += () => { button.interactable = false; };
+            ad.OnLoadingStart += () =>
+            {
+                button.interactable = false;
+                OnLoadingStart?.Invoke();
+                OnDisabled?.Invoke();
+            };
+            ad.OnLoadingComplete += () =>
+            {
+                button.interactable = true;
+                OnLoadingComplete?.Invoke();
+                OnInteractive?.Invoke();
+            };
+            ad.OnStartWatch += () =>
+            {
+                button.interactable = false;
+                OnStartWatch?.Invoke();
+                OnDisabled?.Invoke();
+            };
 
             button.onClick.AddListener(ShowAd);
             button.interactable = ad.Loaded();
+            if (button.interactable)
+            {
+                OnInteractive?.Invoke();
+            }
+            else
+            {
+                OnDisabled?.Invoke();
+            }
         }
 
         void ShowAd()
